4d67918670b830ee5463f68d4b70d74516aad2a9
Jaydon Lee <hjjaydon.lee@gmail.com>
2022-10-23 16:34:24 -0700

    design: designing splash page

--- pages/SplashPage.js
+++ pages/SplashPage.js
@@ -1,9 +1,36 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  screen: {
+    flex: 1,
+    flexDirection: "column",
+    border: "1px none",
+    alignItems: "center",
+    textDecorationColor: "none",
+  },
+  title: {
+    minHeight: "42px",
+    marginTop: "39px",
+  },
+});
 
 const SplashPage = () => {
   return (
-    <View>
-      <Text>Welcome to Navigation Page</Text>
+    <View style={styles.container}>
+      <View style={styles.screen}>
+        <Text style={styles.title}>Welcome to Toad</Text>
+        <Text>
+          simply dummy text of the printing and typesetting indudddsstry. Lorem
+          Ipsum
+        </Text>
+      </View>
     </View>
   );
 };
