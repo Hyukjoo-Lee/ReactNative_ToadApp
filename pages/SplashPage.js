371f39c73c7b1bdae1e2c268c87a5a7b92072807
Jaydon Lee <hjjaydon.lee@gmail.com>
2022-10-25 15:04:04 -0700

    comment: explain functions

--- pages/SplashPage.js
+++ pages/SplashPage.js
@@ -1,6 +1,7 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { SliderBox } from "react-native-image-slider-box";
+import { useNavigation } from "@react-navigation/native";
 
 const styles = StyleSheet.create({
   container: {
@@ -54,12 +55,23 @@ const styles = StyleSheet.create({
 });
 
 const SplashPage = () => {
+  const navigation = useNavigation();
+
+  // Images in the image slider: should be replaced with others
   const images = [
     "https://source.unsplash.com/1024x768/?nature",
     "https://source.unsplash.com/1024x768/?water",
     "https://source.unsplash.com/1024x768/?girl",
     "https://source.unsplash.com/1024x768/?tree",
   ];
+
+  // Move to Signup page
+  const handleSignUp = () => {
+    navigation.navigate("SignUp");
+  };
+
+  // Move to Login page
+  const handleLogin = () => {};
 
   return (
     <View style={styles.container}>
@@ -73,16 +85,23 @@ const SplashPage = () => {
           <SliderBox
             images={images}
             onCurrentImagePressed={(index) =>
+              // it moves to proper pages when a image is clicked
               console.warn(`image ${index} pressed`)
             }
           />
         </View>
         <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Create new account</Text>
+          <Text style={styles.buttonText} onPress={handleSignUp}>
+            Create new account
+          </Text>
         </TouchableOpacity>
         <Text style={styles.loginText}>
           Do you have an accout?{" "}
-          <Text style={{ fontWeight: "bold" }}>Log In</Text>
+          <TouchableOpacity>
+            <Text onPress={handleLogin} style={{ fontWeight: "bold" }}>
+              Log In
+            </Text>
+          </TouchableOpacity>
         </Text>
       </View>
     </View>
