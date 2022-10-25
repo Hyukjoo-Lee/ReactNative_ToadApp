4c13707b8117103440107f8f367fd22651b36496
Jaydon Lee <hjjaydon.lee@gmail.com>
2022-10-25 14:22:25 -0700

    design: designed splash page with a image slider

--- pages/SplashPage.js
+++ pages/SplashPage.js
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+import { SliderBox } from "react-native-image-slider-box";
 
 const styles = StyleSheet.create({
   container: {
@@ -16,19 +17,72 @@ const styles = StyleSheet.create({
     textDecorationColor: "none",
   },
   title: {
-    minHeight: "42px",
-    marginTop: "39px",
+    marginBottom: "3%",
+    opacity: "0.6",
+    fontWeight: "500",
+    fontSize: "36px",
+  },
+  description: {
+    opacity: "0.6",
+    fontSize: "16px",
+    width: "75%",
+    minheight: "4%",
+    textAlign: "center",
+    marginBottom: "5.5%",
+  },
+  imageContainer: {
+    // image size should be edited
+    width: "100%",
+  },
+
+  button: {
+    backgroundColor: "lightgray",
+    width: "85%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 40,
+  },
+  buttonText: {
+    color: "black",
+    fontSize: 16,
+  },
+  loginText: {
+    marginTop: 20,
+    fontSize: 16,
   },
 });
 
 const SplashPage = () => {
+  const images = [
+    "https://source.unsplash.com/1024x768/?nature",
+    "https://source.unsplash.com/1024x768/?water",
+    "https://source.unsplash.com/1024x768/?girl",
+    "https://source.unsplash.com/1024x768/?tree",
+  ];
+
   return (
     <View style={styles.container}>
       <View style={styles.screen}>
         <Text style={styles.title}>Welcome to Toad</Text>
-        <Text>
-          simply dummy text of the printing and typesetting indudddsstry. Lorem
+        <Text style={styles.description}>
+          simply dummy text of the printing and typesetting industry. Lorem
           Ipsum
+        </Text>
+        <View style={styles.imageContainer}>
+          <SliderBox
+            images={images}
+            onCurrentImagePressed={(index) =>
+              console.warn(`image ${index} pressed`)
+            }
+          />
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Create new account</Text>
+        </TouchableOpacity>
+        <Text style={styles.loginText}>
+          Do you have an accout?{" "}
+          <Text style={{ fontWeight: "bold" }}>Log In</Text>
         </Text>
       </View>
     </View>
