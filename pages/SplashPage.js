fa8745f4e4cde523ff9949a85b34fa6aae89d812
Jaydon Lee <hjjaydon.lee@gmail.com>
2022-10-25 17:01:37 -0700

    design: designing signup page

--- pages/SplashPage.js
+++ pages/SplashPage.js
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { SliderBox } from "react-native-image-slider-box";
 import { useNavigation } from "@react-navigation/native";
