e4c86536330e55daff69f45fcbd03a482c14761e
csis2020 <73615176+csis2020@users.noreply.github.com>
2022-11-10 14:05:40 -0800

    [feat] add main page

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import PasswordPage from "./pages/PasswordPage";
 import SignUpPage from "./pages/SignupPage";
 // import HomePage from "./pages/HomePage";
 // import LoginPage from "./pages/LoginPage";
+import MainPage from "./pages/MainPage";
 import "expo-dev-client";
 
 import SplashPage from "./pages/SplashPage";
@@ -14,15 +15,18 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+
         <Stack.Screen name="SignUp" component={SignUpPage} />
         <Stack.Screen name="Password" component={PasswordPage} />
         <Stack.Screen name="Splash" component={SplashPage} />
-        {/* <Stack.Screen
+
+        <Stack.Screen name="Main" component={MainPage} />
+        <Stack.Screen
           options={{ headerShown: false }}
           name="Login"
           component={LoginPage}
         />
-        <Stack.Screen name="Home" component={HomePage} /> */}
+        <Stack.Screen name="Home" component={HomePage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
