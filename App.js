6aca99d225d9ed58a3e180e9bea5382ca2db9a68
Jaydon Lee <hjjaydon.lee@gmail.com>
2022-11-10 14:47:07 -0800

    fixed: duplicated home page declared

--- App.js
+++ App.js
@@ -8,12 +8,9 @@ import MainPage from "./pages/MainPage";
 import "expo-dev-client";
 
 import SplashPage from "./pages/SplashPage";
-import HomePage from "./pages/HomePage";
-
 
 // 20221108 Add Tabs by Jessi
 import Tabs from "./Navigation/Tabs";
-
 
 const Stack = createNativeStackNavigator();
 
@@ -21,20 +18,18 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tabs />
-        {/* <Stack.Navigator>
-          <Stack.Screen name="Main" component={MainPage} />
 
-          <Stack.Screen name="Splash" component={SplashPage} />
-          <Stack.Screen name="SignUp" component={SignUpPage} />
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="Login"
-            component={LoginPage}
-          />
-          <Stack.Screen name="Password" component={PasswordPage} />
-          <Stack.Screen name="Home" component={HomePage} />
-        </Stack.Navigator> */}
+      {/* <Stack.Screen name="Main" component={MainPage} />
 
+      <Stack.Screen name="Splash" component={SplashPage} />
+      <Stack.Screen name="SignUp" component={SignUpPage} />
+      <Stack.Screen
+        options={{ headerShown: false }}
+        name="Login"
+        component={LoginPage}
+      />
+      <Stack.Screen name="Password" component={PasswordPage} />
+      <Stack.Screen name="Home" component={HomePage} /> */}
     </NavigationContainer>
   );
 }
