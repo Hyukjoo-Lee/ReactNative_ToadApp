0f09609392f62ac7a7eefbf48c41af1117de7534
csis2020 <73615176+csis2020@users.noreply.github.com>
2022-12-22 16:04:57 -0800

    [feat] Add dropdownpicker for distance

--- pages/CategoryPage.js
+++ pages/CategoryPage.js
@@ -1,16 +1,32 @@
 // 20221109 Add CategoryPage by Jessi
 
+import { useState } from "react";
 import { View, StyleSheet, Text, TextInput, Image } from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
 
 /**
  * CategoryPage by Jessi
  */
 const CategoryPage = () => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    { label: "Apple", value: "apple" },
+    { label: "Banana", value: "banana" },
+  ]);
+
   return (
     <View>
       <Text> Hello CategoryPage :D </Text>
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+      />
     </View>
-
     // <NavigationContainer>
     //  <Tab.Navigator
     //   tabBarOptions={{
